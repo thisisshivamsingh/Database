05958f8f6e2463a1ef137998e2fe250dccfde7c8
Shivam <thisisshivam06@gmail.com>
2022-07-20 16:06:09 +0530

    Model and Schema

--- server.js
+++ server.js
@@ -16,40 +16,70 @@
 
 /////////////// THE MONGO DRIVER /////////////
 
+// const express = require("express");
+// const app = express();
+// const { MongoClient } = require("mongodb");
+
+// const mongoUri =
+//   "mongodb+srv://shivam8976:<password>@cluster0.wmqxy3u.mongodb.net/?retryWrites=true&w=majority";
+
+// const client = new MongoClient(mongoUri);
+
+// app.get("/api/users", async (req, res) => {
+//   try {
+//     await client.connect();
+//     const database = client.db("myApp");
+//     const collection = database.collection("users");
+//     const query = await collection.insertOne({
+//       name: "Francis",
+//       lastname: "Jones",
+//     });
+//     console.log(query);
+//     res.status(200).json({ awesome: "yes" });
+//   } catch (error) {
+//     throw error;
+//   } finally {
+//     await client.close();
+//     console.log("all is done");
+//   }
+// });
+
+// // MongoClient.connect(mongoUri, (err, client) => {
+// //   if (err) {
+// //     throw err;
+// //   }
+// //   console.log("Connected to the DB");
+// // });
+
+// const port = process.env.POST || 3001;
+// app.listen(port);
+
+// /// shivam8976
+// /// qWaQmlA0FpAVPArV
+
+/////////////// MODAL AND SCHEMA /////////////
+
 const express = require("express");
 const app = express();
-const { MongoClient } = require("mongodb");
+const mongoose = require("mongoose");
 
 const mongoUri =
-  "mongodb+srv://shivam8976:<password>@cluster0.wmqxy3u.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://shivam8976:<password>@cluster0.wmqxy3u.mongodb.net/myApp?retryWrites=true&w=majority";
 
-const client = new MongoClient(mongoUri);
+mongoose.connect(mongoUri);
 
-app.get("/api/users", async (req, res) => {
-  try {
-    await client.connect();
-    const database = client.db("myApp");
-    const collection = database.collection("users");
-    const query = await collection.insertOne({
-      name: "Francis",
-      lastname: "Jones",
-    });
-    console.log(query);
-    res.status(200).json({ awesome: "yes" });
-  } catch (error) {
-    throw error;
-  } finally {
-    await client.close();
-    console.log("all is done");
-  }
+///////// SCHEMA /////////
+
+const carSchema = mongoose.Schema({
+  brand: String,
+  model: String,
+  year: Number,
+  avail: Boolean,
 });
 
-// MongoClient.connect(mongoUri, (err, client) => {
-//   if (err) {
-//     throw err;
-//   }
-//   console.log("Connected to the DB");
-// });
+const Car = mongoose.model("Car", carSchema);
+
+/////////////////////////
 
 const port = process.env.POST || 3001;
 app.listen(port);
